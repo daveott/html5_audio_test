341cb7c03e574418c2e9d8bc909a1f94c2c579d1
Hashrocket Workstation <dev@hashrocket.com>
2011-10-18 23:20:06 -0400

    Install rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5AudioTest::Application.config.secret_token = '19ca2c6e34da21dc4dd03de3372a82adb9251daca19da1a5861b676d88356da4222674a61ddfa6444e52045b7235f9ae01d432455731b39244fb7ef094889357'
